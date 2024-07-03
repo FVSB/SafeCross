10a364f2a0ff725af8ac315fcd74202084c526f3
Kitsulee <lauradi1410@gmail.com>
2024-07-03 17:13:40 -0400

    Few epochs train code

---
+++ train.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import os
+
+current_path = os.getcwd()
+# Cargar el modelo pre-entrenado YOLOv8m
+model_path = os.path.join(os.path.dirname(__file__), 'yolov8m.pt')
+model = YOLO(model_path)
+
+# Realizar fine-tuning
+results = model.train(
+    data=os.path.join(current_path,'TrafficLight.yaml'),
+    epochs=5,
+    imgsz=640,
+    batch=16,
+    name='yolov8m_traffic_light'
+)
+
+# Guardar el modelo entrenado
+model.save('yolov8m_TrafficLight.pt')
